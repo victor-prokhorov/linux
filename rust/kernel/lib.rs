a602eb7948e65cee8c94524cee0751e8ca9f31eb
Victor Prokhorov <mail@victorprokhorov.com>
2026-03-31 16:49:48 +0200

    rust: add `ptr_as_ref_unchecked` to `rust_allowed_features`
    Add `ptr_as_ref_unchecked` to the list of allowed Rust unstable
    features. This feature provides `as_ref_unchecked()` and
    `as_mut_unchecked()` on raw pointers, which are the named counterparts
    to the `&*ptr` and `&mut *ptr` patterns.
    
    The feature has been available since Rust 1.80.0 and was stabilized in
    Rust 1.95.0.
    
    Suggested-by: Miguel Ojeda <ojeda@kernel.org>
    Link: https://github.com/Rust-for-Linux/linux/issues/1225
    Signed-off-by: Victor Prokhorov <mail@victorprokhorov.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -16,6 +16,9 @@
 // Please see https://github.com/Rust-for-Linux/linux/issues/2 for details on
 // the unstable features in use.
 //
+// Stable since Rust 1.95.0.
+#![feature(ptr_as_ref_unchecked)]
+//
 // Expected to become stable.
 #![feature(arbitrary_self_types)]
 #![feature(derive_coerce_pointee)]
